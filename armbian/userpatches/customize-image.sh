2e767751d6dddc27d5cef4e0854e83d3ff0f30e2
Maxime Mouchet <max@maxmouchet.com>
2022-07-06 14:20:14 +0200

    Update customize-image.sh

--- armbian/userpatches/customize-image.sh
+++ armbian/userpatches/customize-image.sh
@@ -7,6 +7,9 @@ rm /root/.not_logged_in_yet
 # Disable zram service: incompatible with k8s.
 echo "ENABLED=false" > /etc/default/armbian-zram-config
 
+# Enable the hardware watchdog.
+echo "RuntimeWatchdogSec=600" >> /etc/systemd/system.conf
+
 # Uncomment to enter a shell during build.
 # bash
 
