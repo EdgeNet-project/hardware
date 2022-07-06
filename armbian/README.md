4775e8c3f40b63140aa84f99dead5b9721a3e05e
Maxime Mouchet <max@maxmouchet.com>
2022-07-06 14:13:20 +0200

    add armbian scripts

---
+++ armbian/README.md
@@ -0,0 +1,8 @@
+# EdgeNet Armbian image
+
+```bash
+git clone --depth 1 https://github.com/armbian/build
+cd build
+# copy userpatches/ to build/userpatches/
+./compile.sh docker edgenet
+```
---
+++ armbian/userpatches/customize-image.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Disable root login.
+passwd -l root
+rm /root/.not_logged_in_yet
+
+# Disable zram service: incompatible with k8s.
+echo "ENABLED=false" > /etc/default/armbian-zram-config
+
+# Uncomment to enter a shell during build.
+# bash
+
+# Run EdgeNet bootstrap script.
+export EDGENET_PLAYBOOK=edgenet-at-home-node-prep.yml
+wget -O /tmp/bootstrap.sh https://raw.githubusercontent.com/EdgeNet-project/node/main/bootstrap.sh
+chmod +x /tmp/bootstrap.sh && /tmp/bootstrap.sh
