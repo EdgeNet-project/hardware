97e1b5af78d5a6de932966d52f5f9dbc98816aec
Maxime Mouchet <max@maxmouchet.com>
2022-07-06 15:07:01 +0200

    odroid-n2: use custom Armbian image

--- boot/odroid-n2.sh
+++ boot/odroid-n2.sh
@@ -3,68 +3,23 @@
 set -ux
 
 LOCAL_URL=/tmp/image.xz
-REMOTE_URL=http://de.eu.odroid.in/ubuntu_22.04lts/N2/ubuntu-22.04-4.9-minimal-odroid-n2-20220622.img.xz
-REMOTE_MD5=026cc5d1e23e1554a2b58783e1c50dec
+REMOTE_URL=ftp://ftp.iris.dioptra.io/Armbian_22.08.0-trunk_Odroidn2_jammy_current_5.10.123_minimal.img.xz
+REMOTE_MD5=82f8aefcbb01285dd5fa8ae3a0e8b106
 
-# 1) Download and write the disk image to the eMMC
-
+# Download and write the disk image to the eMMC
 wget -O ${LOCAL_URL} ${REMOTE_URL}
 LOCAL_MD5=$(md5sum ${LOCAL_URL} | awk '{print $1}')
 
 if [ "${LOCAL_MD5}" != "${REMOTE_MD5}" ]; then
 	echo "invalid hash"
-	sleep 10
-	exit 1
+	sleep 60
+	# Reboot
+	echo 1 > /proc/sys/kernel/sysrq
+	echo b > /proc/sysrq-trigger
 fi
 
 xzcat ${LOCAL_URL} | dd conv=fsync bs=500M of=/dev/mmcblk0
 rm ${LOCAL_URL}
 
-# 2) Mount the root partition
-
-partprobe
-mkdir /tmp/root
-mount /dev/mmcblk0p2 /tmp/root
-
-# 3) Install the firstboot service
-
-wget -O /tmp/root/bootstrap.sh https://raw.githubusercontent.com/EdgeNet-project/node/main/bootstrap.sh
-chmod +x /tmp/root/bootstrap.sh
-
-cat > /tmp/root/etc/systemd/system/edgenet-firstboot.service <<END
-[Unit]
-Description=EdgeNet first boot
-After=network.target
-
-[Service]
-ExecStart=/bootstrap.sh
-Type=oneshot
-RemainAfterExit=yes
-Restart=on-failure
-RestartSec=15s
-# Join the EdgeNet@home cluster
-Environment="EDGENET_PLAYBOOK=edgenet-at-home-node.yml"
-
-[Install]
-WantedBy=multi-user.target
-END
-
-ln -fs /etc/systemd/system/edgenet-firstboot.service /tmp/root/etc/systemd/system/multi-user.target.wants/edgenet-firstboot.service
-
-# 4) Enable the hardware watchdog
-
-echo "RuntimeWatchdogSec=600" >> /tmp/root/etc/systemd/system.conf
-
-# 5) Disable root SSH login
-
-echo "PermitRootLogin no" >> /tmp/root/etc/ssh/sshd_config
-
-# 6) Prevent unattended-upgrades from running on first boot
-
-cat <<EOF > /tmp/root/etc/apt/apt.conf.d/20auto-upgrades
-APT::Periodic::Update-Package-Lists "0";
-APT::Periodic::Unattended-Upgrade "0";
-EOF
-
-# 7) Give some time to review the output and boot
+# Give some time to review the output before booting
 sleep 5
